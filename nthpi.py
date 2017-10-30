4c2003e7e0ba08d5c0f03fbf6cfe975713b2066c
Jan Tabaczynski <roflbot101@gmail.com>
2017-10-30 09:21:53 -0400

    implemented flexible precision pi for edge cases

--- nthpi.py
+++ nthpi.py
@@ -2,13 +2,6 @@ from datetime import date
 import random
 
 
-def number_to_digit_list(number):
-    digit_list = list(str(number))
-    if '.' in digit_list:
-        digit_list.remove('.')
-    map(int,digit_list)
-    return digit_list
-
 def get_pi(precision):
     try:
         # import version included with old SymPy
@@ -21,6 +14,13 @@ def get_pi(precision):
     mp.dps = precision  # set number of digits
     return mp.pi
 
+def number_to_digit_list(number):
+    digit_list = list(str(number))
+    if '.' in digit_list:
+        digit_list.remove('.')
+    map(int,digit_list)
+    return digit_list
+
 def find_sequence_in_list(base_list, sequence):
     for i in range(len(base_list)):
         for j in range(len(sequence)):
@@ -31,26 +31,28 @@ def find_sequence_in_list(base_list, sequence):
     return -1
 
 def write_message():
-    '''
-      TODO: Run again with a higher get_pi(digit) if 
-      find_sequence_in_list returns -1 until a result is found
-    '''
-
-    pi = get_pi(20000)
-    pi_digits = number_to_digit_list(pi)
-    
     start_date = date.today().replace(day=1, month=1).toordinal()
     end_date = date.today().toordinal()
     random_date = date.fromordinal(random.randint(start_date, end_date))
     formatted_date = int(str(random_date.month) + str(random_date.day))
     date_digits = number_to_digit_list(formatted_date)
     
-    n = find_sequence_in_list(pi_digits, date_digits)
-    d = random_date.strftime('%B %-d')
+    precision = 20000
+    pi = get_pi(precision)
+    pi_digits = number_to_digit_list(pi)
     
+    d = random_date.strftime('%B %-d')
+    n = find_sequence_in_list(pi_digits, date_digits)
+
+    while n < 0:
+        precision *= 2
+        pi = get_pi(precision)
+        pi_digits = number_to_digit_list(pi)
+        n = find_sequence_in_list(pi_digits, date_digits)
+
     s = ''
     if n % 10 == 1:
-        s = 'th'
+        s = 'st'
     elif n % 10 == 2:
         s = 'nd'
     elif n % 10 == 3:
